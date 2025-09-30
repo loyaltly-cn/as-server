d8dc4665b76935977ac8459f375f19f23678322b
loyal <102851403+loyaltly-cn@users.noreply.github.com>
2025-09-30 17:19:24 +0800

    update

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { Hono } from "hono"
+import { loadRoutes } from "./lib/router"
+import path from "path"
+
+const app = new Hono()
+const apiDir = path.join(process.cwd(), "api")
+loadRoutes(app, apiDir)
+
+export default app
